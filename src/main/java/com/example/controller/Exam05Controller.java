c2080faf3eb7dd93896db4fc2df3bdb1c9f6f215
takuto-tsukigata <takuto.tsukigata@rakus-partners.co.jp>
2022-02-04 16:04:51 +0900

    問５を追加しました。

---
+++ src/main/java/com/example/controller/Exam05Controller.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Member;
+import com.example.repository.MemberRepository;
+
+@Controller
+@RequestMapping("/exam05")
+public class Exam05Controller {
+
+	@Autowired
+	private MemberRepository repository;
+
+	@RequestMapping("")
+	public String index() {
+		return "exam05";
+	}
+
+	@RequestMapping("/result")
+	public String findByName(String name, Model model) {
+
+		model.addAttribute("name", repository.findByName(name));
+
+		List<Member> nameList = repository.findByName(name);
+//				.stream().map(s -> s.getName()).collect(Collectors.toList());
+
+		model.addAttribute("names", nameList);
+
+		return "exam05-result";
+	}
+}
---
+++ src/main/java/com/example/domain/Member.java
@@ -0,0 +1,46 @@
+package com.example.domain;
+
+public class Member {
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getDepId() {
+		return depId;
+	}
+
+	public void setDepId(Integer depId) {
+		this.depId = depId;
+	}
+
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Integer depId;
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
